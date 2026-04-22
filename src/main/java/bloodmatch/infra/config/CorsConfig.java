8646c80afb8fe2f21cc25c517bf300fab7030825
Gabriel <gardengab@outlook.com>
2026-04-22 03:51:13 -0300

    CORS to github pages added. front gonna consume it.

--- src/main/java/bloodmatch/infra/config/CorsConfig.java
+++ src/main/java/bloodmatch/infra/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
-                .allowedOrigins("http://localhost:5173") // local vite development server
+                .allowedOrigins("http://localhost:5173", "https://gabgarden.github.io/") // local vite development server
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
